b5232404b296124496e5832f66fd2edde8bd1b2d
Vahflouz <v.morin59@gmail.com>
2019-05-17 14:20:25 +0200

    Some improvements

--- Script_Analyse.ps1
+++ Script_Analyse.ps1
@@ -1,20 +1,21 @@
 ﻿param
 (
-   [String]
-   [Parameter(Mandatory)]
-   $Title
+   [String]$Title,
+   [String]$Root_path
 )
-Start-Process $Title #debug
+
+#Start-Process $Title #debug
 $log = ""
 $process = Get-Process -Name $Title
 $DateDebut = Get-Date
 
-$path = "workingFiles\"
+$path = "$Root_path\workingFiles\"
+
 If(!(test-path $path))
 {
       New-Item -ItemType Directory -Force -Path $path
 }
-$file = "workingFiles\" + $Title.ToString() + $DateDebut.ToString("yyyyMMddhhmmss") + ".txt"
+$file = $path + $Title.ToString() + $DateDebut.ToString("yyyyMMddhhmmss") + ".txt"
 "Date of process start : " + $DateDebut.ToString()  > $file
 "ProcessName`tCPU`n" >> $file
 
@@ -32,6 +33,7 @@ while ($process)
         break
     }
 }
+
 $DateFin = Get-Date
 "Date of process end : " + $DateFin.ToString() >> $file
 
--- Script_Only_One.ps1
+++ Script_Only_One.ps1
@@ -3,7 +3,12 @@
 # Variable to infinite loop
 $olive = 1
 
+Remove-Job *
+
 $job_running = $null
+$root_path = Get-Location
+$root_path = $root_path.ToString()
+$root_path
 
 do {
 
@@ -19,25 +24,28 @@ do {
             if ($job_running -eq $null) {
 
                 #$app = Start-Process powershell -ArgumentList ".\Script_Analyse.ps1 -Title $_" -PassThru -UseNewEnvironment
-                Start-Job -Name $_"_analyser" -FilePath {.\Script_Analyse.ps1} -ArgumentList $_
+                Start-Job -Name $_"_analyser" -FilePath .\Script_Analyse.ps1 -ArgumentList @($_,$root_path)
                 
             }
 
         } else {
 
             Echo "$_ inactif"
+            
+            $job_running = Get-Job -Name $_"_analyser" -ErrorAction SilentlyContinue
+
+            if ($job_running -ne $null -and $job_running.State -ne "Running") {
+                
+                Remove-Job -Name $_"_analyser"
+                Write-Host "Job removed"
+                
+            }
+
             Set-Variable -Name $_"_status" -Value 0
             $running = 0
             $job_running = $null
 
         }
-
-        # Get-Variable -Name $_"_status" -ValueOnly
-
-        # Start-Process notepad -Passthru
-        # $total = $total + $process
-
-        #echo $total
 
         Start-Sleep -Seconds 1
         
